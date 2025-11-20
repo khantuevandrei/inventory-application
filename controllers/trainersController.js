4f0367e4d4f8ef667267e0f905f6e4cc3764e4ad
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-20 13:16:12 +0500

    Add route, method to handle new trainer creation

--- controllers/trainersController.js
+++ controllers/trainersController.js
@@ -10,6 +10,44 @@ async function trainersListGet(req, res) {
   });
 }
 
+function createTrainerGet(req, res) {
+  res.render("create-trainer", {
+    title: "Create trainer",
+  });
+}
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateTrainer = [
+  body("newTrainer")
+    .trim()
+    .isAlpha()
+    .withMessage(`Name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Name ${lengthErr}`),
+];
+
+createTrainerPost = [
+  validateTrainer,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("create-trainer", {
+        title: "Create trainer",
+        errors: errors.array(),
+      });
+    }
+
+    const { newTrainer } = matchedData(req);
+    await db.createNewTrainer(newTrainer);
+    res.redirect("/trainers");
+  },
+];
+
 module.exports = {
   trainersListGet,
+  createTrainerGet,
+  createTrainerPost,
 };
--- routes/trainersRouter.js
+++ routes/trainersRouter.js
@@ -4,5 +4,7 @@ const trainersController = require("../controllers/trainersController");
 const trainersRouter = Router();
 
 trainersRouter.get("/", trainersController.trainersListGet);
+trainersRouter.get("/create", trainersController.createTrainerGet);
+trainersRouter.post("/create", trainersController.createTrainerPost);
 
 module.exports = trainersRouter;
