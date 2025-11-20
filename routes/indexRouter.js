bb483d90ede3fd1c8df24d30f5fde4df7945d5ba
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-20 11:40:43 +0500

    Create post controller to handle new type creation, update router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,7 @@ const inventoryController = require("../controllers/inventoryController");
 const indexRouter = Router();
 
 indexRouter.get("/", inventoryController.typesListGet);
+indexRouter.get("/createtype", inventoryController.createTypeGet);
+indexRouter.post("/createtype", inventoryController.createTypePost);
 
 module.exports = indexRouter;
