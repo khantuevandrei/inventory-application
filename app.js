252838c27005e3f98e85f920ba23fe4e8100ddf3
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-20 12:40:04 +0500

    Add new routes to app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const typesRouter = require("./routes/typesRouter");
+const trainersRouter = require("./routes/trainersRouter");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/types", typesRouter);
+app.use("/trainers", trainersRouter);
 
 app.use((err, req, res, next) => {
   console.log(err);
